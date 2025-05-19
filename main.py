ee42e17ccc337daead5b9da28d5e946574298e04
dandleion <129268666+dandleion@users.noreply.github.com>
2025-05-19 21:39:42 +0900

    Update main.py

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ st.markdown("""
             transform: scaleX(1);
         }
         50% {
-            left: calc(100vw - 700px);
+            left: calc(100vw - 1000px);
             transform: scaleX(1);
         }
         51% {
